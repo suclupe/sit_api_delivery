5cf91b4b6e401d5ac653dad8de3d5bc3a5315ff4
Suclupe <jsuclupev1@upao.edu.pe>
2023-04-21 11:03:56 -0500

    Crear la clase Order con los atributos id, customerName, customerEmail, status, creationTime, estimatedDeliveryTime y items.

--- api_delivery/src/main/java/com/delivery/api_delivery/entities/Order.java
+++ api_delivery/src/main/java/com/delivery/api_delivery/entities/Order.java
@@ -1,4 +1,82 @@
 package com.delivery.api_delivery.entities;
 
+import java.util.Date;
+import java.util.List;
+
 public class Order {
+    private int id;
+    private String customerName;
+    private String customerEmail;
+    private String status;
+    private Date creationTime;
+    private Date estimatedDeliveryTime;
+    private List<Food> items;
+
+    public Order(int id, String customerName, String customerEmail, String status, Date creationTime, Date estimatedDeliveryTime, List<Food> items) {
+        this.id = id;
+        this.customerName = customerName;
+        this.customerEmail = customerEmail;
+        this.status = status;
+        this.creationTime = creationTime;
+        this.estimatedDeliveryTime = estimatedDeliveryTime;
+        this.items = items;
+    }
+
+    // Getters and Setters
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerEmail() {
+        return customerEmail;
+    }
+
+    public void setCustomerEmail(String customerEmail) {
+        this.customerEmail = customerEmail;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Date getCreationTime() {
+        return creationTime;
+    }
+
+    public void setCreationTime(Date creationTime) {
+        this.creationTime = creationTime;
+    }
+
+    public Date getEstimatedDeliveryTime() {
+        return estimatedDeliveryTime;
+    }
+
+    public void setEstimatedDeliveryTime(Date estimatedDeliveryTime) {
+        this.estimatedDeliveryTime = estimatedDeliveryTime;
+    }
+
+    public List<Food> getItems() {
+        return items;
+    }
+
+    public void setItems(List<Food> items) {
+        this.items = items;
+    }
 }
