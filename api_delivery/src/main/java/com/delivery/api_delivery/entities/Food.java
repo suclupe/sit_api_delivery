55fc9cdc05c54adf96e451b05e129ee60fee8994
Suclupe <jsuclupev1@upao.edu.pe>
2023-04-21 11:01:22 -0500

    Crear la clase Food con los atributos name, description y price

--- api_delivery/src/main/java/com/delivery/api_delivery/entities/Food.java
+++ api_delivery/src/main/java/com/delivery/api_delivery/entities/Food.java
@@ -1,4 +1,39 @@
 package com.delivery.api_delivery.entities;
 
 public class Food {
+    private String name;
+    private String description;
+    private double price;
+
+    public Food(String name, String description, double price) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+
+    // Getters and Setters
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
