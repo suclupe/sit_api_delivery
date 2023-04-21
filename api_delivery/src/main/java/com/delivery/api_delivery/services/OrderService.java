12c72dc00ce5faf217cd5dfcdfd81c1a5de6cb48
Suclupe <jsuclupev1@upao.edu.pe>
2023-04-21 11:07:13 -0500

    Crear la clase OrdenService para manejar la logica de negocios de los pedidos

--- api_delivery/src/main/java/com/delivery/api_delivery/services/OrderService.java
+++ api_delivery/src/main/java/com/delivery/api_delivery/services/OrderService.java
@@ -1,4 +1,29 @@
 package com.delivery.api_delivery.services;
 
+import com.delivery.api_delivery.entities.Order;
+import java.util.List;
+
 public class OrderService {
+    private List<Order> orders;
+
+    public OrderService(List<Order> orders) {
+        this.orders = orders;
+    }
+
+    public void addOrder(Order order) {
+        orders.add(order);
+    }
+
+    public Order getOrder(int id) {
+        for (Order order : orders) {
+            if (order.getId() == id) {
+                return order;
+            }
+        }
+        return null;
+    }
+
+    public List<Order> getAllOrders() {
+        return orders;
+    }
 }
